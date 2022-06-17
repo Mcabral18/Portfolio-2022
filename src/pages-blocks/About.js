c632ebea840830284c22bca83c66fafc5f7b301b
Marco Cabral <cabralovil@gmail.com>
2022-06-17 21:11:26 +0100

    About

--- src/pages-blocks/About.js
+++ src/pages-blocks/About.js
@@ -3,7 +3,7 @@
 function About() {
     return (
         <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
-            <h1>about</h1>
+
         </div>
     );
 }
